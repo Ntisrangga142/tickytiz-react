92a6e509cf78572f88c5d2f507a9cbf5ff4c697b
Titus Rangga <titusrangga142@gmail.com>
2025-07-30 03:14:31 +0700

    feat: add page components for login, signup, and movie

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,15 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+// import Login from "./pages/Login.jsx";
+// import Register from './pages/Register.jsx';
+import Movie from './pages/Movie'
+
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    {/* <Login  /> */}
+    {/* <Register /> */}
+    <Movie />
   </StrictMode>,
 )
