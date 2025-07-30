89c8e2ee3b50e8d89dc167244776e5d73c68585a
Titus Rangga <titusrangga142@gmail.com>
2025-07-31 01:58:16 +0700

    feat: add Button for submit for login and register page

--- src/atoms/ButtonSR.jsx
+++ src/atoms/ButtonSR.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function ButtonSR({type, msg}) {
   return (
-    <button type={type}>{msg}</button>
+    <button type={type} className="text-[#F7F7FC] bg-[#1D4ED8] w-full h-[64px] rounded text-base font-bold border-none mt-[26px]">{msg}</button>
   )
 }
 
