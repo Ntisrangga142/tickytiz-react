e20878c33467840ea6e0ceb5542060ee6db4f5c7
Titus Rangga <titusrangga142@gmail.com>
2025-07-31 09:57:45 +0700

    feat: add Navigation with react-router

--- src/organism/SignInUp.jsx
+++ src/organism/SignInUp.jsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router";
 
 function SignInUp() {
   const aClass1 = "border border-[#1D4ED8] text-[#1D4ED8] font-normal text-sm tracking-[0.75px] rounded-[5px] no-underline px-[18px] py-[12px]"
@@ -5,8 +6,8 @@ function SignInUp() {
 
   return (
     <div className="flex flex-row justify-between gap-[6px]">
-      <a className={aClass1}>Sign In</a>
-      <a className={aClass2}>Sign Up</a>
+      <Link className={aClass1} to='/login'>Sign In</Link>
+      <Link className={aClass2} to='/register'>Sign Up</Link>
     </div>
   );
 }
