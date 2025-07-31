3b95726d476d96d7f965d8d4f31211a7c34d41b1
Titus Rangga <titusrangga142@gmail.com>
2025-07-31 09:55:48 +0700

    fix: apply react-router for ticktiz web

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-// import Login from "./pages/Login.jsx";
-// import Register from './pages/Register.jsx';
-import Movie from './pages/Movie'
+
+import './styles/index.css'
+import Routing from './Routing'
 
 
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    {/* <Login  /> */}
-    {/* <Register /> */}
-    <Movie />
-  </StrictMode>,
+    <Routing />
+  </StrictMode>
 )
